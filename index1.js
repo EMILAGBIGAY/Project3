851cf570e0a7708d895329a257f6b8fd1938c9e3
Ashwin Kundeti <96263343+ashwin-k22@users.noreply.github.com>
2023-04-17 19:30:06 -0500

    index1 has necessary backend functions

---
+++ index1.js
@@ -0,0 +1,185 @@
+const express = require('express')
+const { Pool } = require('pg');
+const dotenv = require('dotenv').config();
+
+// Create Express App
+const app = express();
+const router = express.Router();
+const port = 3000;
+
+//Create Postgres Pool
+const pool = new Pool({
+    user: process.env.PSQL_USER,
+    host: process.env.PSQL_HOST,
+    database: process.env.PSQL_DATABASE,
+    password: process.env.PSQL_PASSWORD,
+    port: process.env.PSQL_PORT,
+    ssl: {rejectUnauthorized: false}
+});
+
+process.on('SIGINT', function() {
+    pool.end();
+    console.log('Application successfully terminated');
+    process.exit(0);
+});
+
+app.set('view engine', "ejs");
+
+// Serve static files from the public directory
+// app.use(express.static('public'));
+
+router.get('/', (req, res) => {
+    res.render('index');
+})
+
+router.get('/user', (req, res) => {
+    arr = [];
+    pool
+        .query('SELECT * FROM inventory')
+        .then(query_res => {
+            for (let i = 0; i < query_res.rowCount; i++) {
+                arr.push(query_res.rows[i]);
+            }
+            const data = {arr: arr};
+            console.log(arr);
+            res.render('user', data);
+        });
+});
+
+router.get('/server', (req, res) => {
+    let coffee_arr = [];
+    let espresso_arr = [];
+    let tea_arr = [];
+    let refresher_arr = [];
+    let coffeefrap_arr = [];
+    let cremefrap_arr = [];
+    let breakfast_arr = [];
+    let bakery_arr = [];
+    pool.query("select * from menu where subcategory='Coffee'")
+        .then(query_res => {
+            for (let i = 0; i < query_res.rowCount; i++) {
+                coffee_arr.push(query_res.rows[i]);
+            }
+            return pool.query("select * from menu where subcategory='Espresso'");
+        })
+        .then(query_res => {
+            for (let i = 0; i < query_res.rowCount; i++) {
+                espresso_arr.push(query_res.rows[i]);
+            }
+            return pool.query("select * from menu where subcategory='Tea'");
+        })
+        .then(query_res => {
+            for (let i = 0; i < query_res.rowCount; i++) {
+                tea_arr.push(query_res.rows[i]);
+            }
+            return pool.query("select * from menu where subcategory='Refresher'");
+        })
+        .then(query_res => {
+            for (let i = 0; i < query_res.rowCount; i++) {
+                refresher_arr.push(query_res.rows[i]);
+            }
+            return pool.query("select * from menu where subcategory='Coffee Frappuccino'");
+        })
+        .then(query_res => {
+            for (let i = 0; i < query_res.rowCount; i++) {
+                coffeefrap_arr.push(query_res.rows[i]);
+            }
+            return pool.query("select * from menu where subcategory='Creme Frappuccino'");
+        })
+        .then(query_res => {
+            for (let i = 0; i < query_res.rowCount; i++) {
+                cremefrap_arr.push(query_res.rows[i]);
+            }
+            return pool.query("select * from menu where subcategory='Breakfast'");
+        })
+        .then(query_res => {
+            for (let i = 0; i < query_res.rowCount; i++) {
+                breakfast_arr.push(query_res.rows[i]);
+            }
+            return pool.query("select * from menu where subcategory='Bakery'");
+        })
+        .then(query_res => {
+            for (let i = 0; i < query_res.rowCount; i++) {
+                bakery_arr.push(query_res.rows[i]);
+            }
+            const data = {
+                coffee_arr: coffee_arr,
+                espresso_arr: espresso_arr,
+                tea_arr: tea_arr,
+                refresher_arr: refresher_arr,
+                coffeefrap_arr: coffeefrap_arr,
+                cremefrap_arr: cremefrap_arr,
+                breakfast_arr: breakfast_arr,
+                bakery_arr: bakery_arr
+            };
+            console.log(data);
+            res.render('server', data);
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+        });
+});
+
+router.get('/manager', (req, res) => {
+    // x, z, excess, sales reports
+    // update/restock inventory
+    // update/add menu item
+    //
+    let inventory_arr = [];
+    pool.query("select * from inventory")
+        .then(query_res => {
+            for (let i = 0; i < query_res.rowCount; i++) {
+                inventory_arr.push(query_res.rows[i]);
+            }
+            const data = {inventory_arr: inventory_arr};
+            console.log(data);
+            res.render('manager', data);
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+        });
+});
+
+router.post('/update-inventory', (req, res) => {
+    const id = req.body.id;
+    pool.query("update inventory set quantity = quantity + 10000 where id = $1", [id])
+        .then(() => {
+            console.log("Item quantity updated");
+            res.status(200).send("Item quantity updated");
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+        });
+});
+
+router.post('/add-inventory-item', (req, res) => {
+    const item = req.body.item;
+    const quantity = req.body.quantity;
+    pool.query("insert into inventory (item, quantity) values ($1, $2)", [item, quantity])
+
+router.post('/add-menu-item', (req, res) => {
+    const category = req.body.category;
+    const subcategory = req.body.subcategory;
+    const item = req.body.item;
+    const tallPrice = req.body.tallPrice;
+    const grandePrice = req.body.grandePrice;
+    const ventiPrice = req.body.ventiPrice;
+    pool.query("insert into menu (category, subcategory, item, tall, grande, venti) values ($1, $2, $3, $4, $5, $6)", [category, subcategory, item, tallPrice, grandePrice, ventiPrice])
+        .then(() => {
+            console.log("Menu item added");
+            res.status(200).send("Menu item added");
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+        });
+});
+
+app.use('/', router);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
