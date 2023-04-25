48e1820a1b07f6fdc29f97c34e5c2e60fd886a92
Ashwin Kundeti <96263343+ashwin-k22@users.noreply.github.com>
2023-04-24 19:33:47 -0500

    Added customer route

--- routes/index.js
+++ routes/index.js
@@ -120,6 +120,39 @@ router.get('/Server/:id', (req, res) => {
         });
 });
 
+router.get('/Customer', (req, res) => {
+    const id = req.params.id;
+    let customerMenu = [];
+    var menuType="Coffee";
+    if(id== "TeaMenu"){
+        menuType = "Tea";
+    } else if(id== "BreakfastMenu" ){
+        menuType = "Breakfast";
+    }else if(id== "BakeryMenu" ){
+        menuType = "Bakery";
+    }else if(id== "CoffeeMenu" ){
+        menuType = "Coffee";
+    }else if(id== "SeasonalMenu" ){
+        menuType = "seasonal";
+    }
+
+    pool.query("select * from menu where subcategory = $1",[menuType])
+        .then(query_res => {
+            for (let i = 0; i < query_res.rowCount; i++) {
+                customerMenu.push(query_res.rows[i]);
+        }
+            const data = {
+                customerMenu: Menu,
+                id: id
+            };
+            console.log(data);
+            res.render('Customer', data);
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+        });
+});
 
 router.get('/Manager', (req, res) => {
     // x, z, excess, sales reports
