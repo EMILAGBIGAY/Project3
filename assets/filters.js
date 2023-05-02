298590d04384c7b7f9c559c5a9f8ca95205846d1
ashwin-k22 <ashwin.kundeti@gmail.com>
2023-05-01 19:02:27 -0500

    styles

--- assets/filters.js
+++
@@ -1,6 +0,0 @@
-const selectEl = document.getElementById("colorblind-select");
-
-selectEl.addEventListener("change", function() {
-  const filter = this.value;
-  document.documentElement.className = filter;
-});
